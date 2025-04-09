aa7cd328d08a798fa749af0ed5c277c1bc5e8117
M. Yusuf Sarıgöz <yusufsarigoz@gmail.com>
2025-04-09 07:23:49 +0000

    make: fix indentation with spaces

--- tests/models/test_base.py
+++ tests/models/test_base.py
@@ -9,29 +9,29 @@ from jax_layers.models.base import BaseConfig, BaseModel
 
 
 class TwoLayerMLPConfig(BaseConfig):
-  dim: int = 4
-  use_bias: bool = False
+    dim: int = 4
+    use_bias: bool = False
 
 class TwoLayerMLP(BaseModel):
-  def __init__(self, config, rngs: nnx.Rngs):
-    self.linear1 = nnx.Linear(config.dim, config.dim, rngs=rngs, use_bias=config.use_bias)
-    self.linear2 = nnx.Linear(config.dim, config.dim, rngs=rngs, use_bias=config.use_bias)
+    def __init__(self, config, rngs: nnx.Rngs):
+        self.linear1 = nnx.Linear(config.dim, config.dim, rngs=rngs, use_bias=config.use_bias)
+        self.linear2 = nnx.Linear(config.dim, config.dim, rngs=rngs, use_bias=config.use_bias)
 
-  def __call__(self, x):
-    x = self.linear1(x)
-    return self.linear2(x)
+    def __call__(self, x):
+        x = self.linear1(x)
+        return self.linear2(x)
 
 def test_save_and_load():
-  ckpt_dir = "/tmp/jaxgarden_test_ckpt"
-  if os.path.exists(ckpt_dir):
-    shutil.rmtree(ckpt_dir)
+    ckpt_dir = "/tmp/jaxgarden_test_ckpt"
+    if os.path.exists(ckpt_dir):
+        shutil.rmtree(ckpt_dir)
 
     config = TwoLayerMLPConfig()
-  model = TwoLayerMLP(config, rngs=nnx.Rngs(0))
-  x = jax.random.normal(jax.random.key(42), (3, 4))
-  assert model(x).shape == (3, 4)
-  state = model.state
-  model.save(ckpt_dir)
-  model_restored = TwoLayerMLP(config, rngs=nnx.Rngs(1)).load(ckpt_dir)
-  state_restored = model_restored.state
-  jax.tree.map(np.testing.assert_array_equal, state, state_restored)
+    model = TwoLayerMLP(config, rngs=nnx.Rngs(0))
+    x = jax.random.normal(jax.random.key(42), (3, 4))
+    assert model(x).shape == (3, 4)
+    state = model.state
+    model.save(ckpt_dir)
+    model_restored = TwoLayerMLP(config, rngs=nnx.Rngs(1)).load(ckpt_dir)
+    state_restored = model_restored.state
+    jax.tree.map(np.testing.assert_array_equal, state, state_restored)
