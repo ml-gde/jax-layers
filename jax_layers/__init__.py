1ba3f28d182e55d0da7b61404d9d16bde3d94c75
Keshan <kesh.jboy@gmail.com>
2025-04-13 20:14:01 +0530

    new: Add text generation utilities with sampling and filtering methods (#22)
    * Add text generation utilities with sampling and filtering methods (without jax.jit)
    
    * fixing mypy issues
    
    * fix jax.jit compilation issue for models

--- jax_layers/__init__.py
+++ jax_layers/__init__.py
@@ -2,8 +2,30 @@
 
 from jax_layers.attention.multi_head_attention import MultiHeadAttention
 from jax_layers.functional.attention import dot_product_attention
+from jax_layers.models.base import BaseConfig, BaseModel
+from jax_layers.models.generation_utils import GenerationMixin
+from jax_layers.models.modernbert import (
+    ModernBertAttention,
+    ModernBertEmbeddings,
+    ModernBERTEncoder,
+    ModernBERTForMaskedLM,
+    ModernBertLayer,
+    ModernBertMLP,
+)
 
 __all__ = [
+    # Base classes
+    "BaseConfig",
+    "BaseModel",
+    # Mixins
+    "GenerationMixin",
+    # Models
+    "ModernBERTEncoder",
+    "ModernBERTForMaskedLM",
+    "ModernBertAttention",
+    "ModernBertEmbeddings",
+    "ModernBertLayer",
+    "ModernBertMLP",
     # Attention modules
     "MultiHeadAttention",
     # Functional interfaces
